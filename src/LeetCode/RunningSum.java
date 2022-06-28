ee42504961cf3b2722ea0a547f15ab0bdc31d612
swapnil7000 <swapnil.bhojwani789@gmail.com>
2022-06-28 23:40:40 +0530

    RunningSum done

---
+++ src/LeetCode/RunningSum.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class RunningSum {
+    public static void main(String[] args) {
+        System.out.println(runningSum(new int[]{1,2,3,4}));
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int resArray[] = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            resArray[i] = sum;
+        }
+        return resArray;
+    }
+}
