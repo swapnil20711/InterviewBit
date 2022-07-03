76a6b262be1f9ae648f9ca29c4384f9b1bdd2935
swapnil7000 <swapnil.bhojwani789@gmail.com>
2022-07-03 19:47:06 +0530

    MaxConsecutiveOnes done

---
+++ src/LeetCode/MaxConsecutiveOnes.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        System.out.println(findMaxConsecutiveOnes(new int[]{1, 1, 0, 1, 1, 1}));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int maxConsecutiveOnes = 0, count = 0;
+        for (int num : nums) {
+            //Increase the count if the number is 1
+            if (num != 0) count++;
+                //else make the count 0
+            else count = 0;
+            //find max of count and maxConsecutiveOnes
+            maxConsecutiveOnes = Math.max(count, maxConsecutiveOnes);
+        }
+        return maxConsecutiveOnes;    }
+}
+
